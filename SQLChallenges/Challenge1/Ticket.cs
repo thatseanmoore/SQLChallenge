16df53c741bfe1c87a6049f12c505f59f9bbcf9a
thatseanmoore <spmoore19@gmail.com>
2021-08-03 21:06:51 -0400

    first commit

---
+++ SQLChallenges/Challenge1/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLChallenges.Challenge1
+{
+    public class Ticket
+    {
+        public int FlightId { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime EstimatedArrivalTime { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public int Capacity { get; set; }
+        public string AirlineName { get; set; }
+    }
+}
---
+++ SQLChallenges/Challenge4/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLChallenges.Challenge4
+{
+    class User
+    {
+    }
+}
